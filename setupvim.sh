b3f4af3fc4c2ed5430d7d3a950f630558ec168e0
Alex Codeing <122148346+alexcode-cc@users.noreply.github.com>
2023-07-25 13:50:02 +0800

    Update setupvim.sh

--- setupvim.sh
+++ setupvim.sh
@@ -13,6 +13,9 @@ git clone https://github.com/alexcode-cc/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Set vimrc
 echo "set nocompatible" > ~/.vimrc && echo "filetype off" >> ~/.vimrc && echo "set rtp+=~/.vim/bundle/Vundle.vim" >> ~/.vimrc && echo "call vundle#begin()" >> ~/.vimrc && echo "Plugin 'alexcode-cc/Vundle.vim'" >> ~/.vimrc && echo "Plugin 'scrooloose/nerdtree'" >> ~/.vimrc && echo "Plugin 'mattn/emmet-vim'" >> ~/.vimrc && echo "call vundle#end()" >> ~/.vimrc && echo "filetype plugin indent on" >> ~/.vimrc  && echo "let g:user_emmet_expandabbr_key = '<tab>'" >> ~/.vimrc  && echo "syntax on" >> ~/.vimrc
 
+# Set Tab format
+echo " set tabstop=2" >> ~/.vimrc  && echo "set softtabstop=2" >> ~/.vimrc  && echo "set shiftwidth=2" >> ~/.vimrc & echo "set noexpandtab" >> ~/.vimrc
+
 # Disable Vim's up / down / left / right key
 echo "map <UP> <NOP>" >> ~/.vimrc && echo "map <DOWN> <NOP>" >> ~/.vimrc && echo "map <LEFT> <NOP>" >> ~/.vimrc  && echo "map <RIGHT> <NOP>" >> ~/.vimrc && echo "inoremap <UP> <NOP>" >> ~/.vimrc  && echo "inoremap <DOWN> <NOP>" >> ~/.vimrc && echo "inoremap <LEFT> <NOP>" >> ~/.vimrc  && echo "inoremap <RIGHT> <NOP>" >> ~/.vimrc
 
