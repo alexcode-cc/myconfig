25f4858dec44b6bc5009d95cf423fd6cac5943ad
Alex Codeing <122148346+alexcode-cc@users.noreply.github.com>
2023-07-25 13:52:44 +0800

    Update setupvim.sh

--- setupvim.sh
+++ setupvim.sh
@@ -14,13 +14,13 @@ git clone https://github.com/alexcode-cc/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "set nocompatible" > ~/.vimrc && echo "filetype off" >> ~/.vimrc && echo "set rtp+=~/.vim/bundle/Vundle.vim" >> ~/.vimrc && echo "call vundle#begin()" >> ~/.vimrc && echo "Plugin 'alexcode-cc/Vundle.vim'" >> ~/.vimrc && echo "Plugin 'scrooloose/nerdtree'" >> ~/.vimrc && echo "Plugin 'mattn/emmet-vim'" >> ~/.vimrc && echo "call vundle#end()" >> ~/.vimrc && echo "filetype plugin indent on" >> ~/.vimrc  && echo "let g:user_emmet_expandabbr_key = '<tab>'" >> ~/.vimrc  && echo "syntax on" >> ~/.vimrc
 
 # Set Tab format
-echo " set tabstop=2" >> ~/.vimrc  && echo "set softtabstop=2" >> ~/.vimrc  && echo "set shiftwidth=2" >> ~/.vimrc & echo "set noexpandtab" >> ~/.vimrc
+echo "set tabstop=2" >> ~/.vimrc  && echo "set softtabstop=2" >> ~/.vimrc  && echo "set shiftwidth=2" >> ~/.vimrc & echo "set noexpandtab" >> ~/.vimrc
 
 # Disable Vim's up / down / left / right key
 echo "map <UP> <NOP>" >> ~/.vimrc && echo "map <DOWN> <NOP>" >> ~/.vimrc && echo "map <LEFT> <NOP>" >> ~/.vimrc  && echo "map <RIGHT> <NOP>" >> ~/.vimrc && echo "inoremap <UP> <NOP>" >> ~/.vimrc  && echo "inoremap <DOWN> <NOP>" >> ~/.vimrc && echo "inoremap <LEFT> <NOP>" >> ~/.vimrc  && echo "inoremap <RIGHT> <NOP>" >> ~/.vimrc
 
 # Map Vim's keys
-echo "nmap <leader>s :w<cr>" >> ~/.vimrc  && echo "nmap <leader>q :q!<cr>" >> ~/.vimrc && echo "nmap <leader>f <C-f>" >> ~/.vimrc  && echo "nmap <leader>b <C-b>" >> ~/.vimrc && echo "nmap <leader>w <C-w>w" >> ~/.vimrc  && echo "nmap <leader>z whdaw" >> ~/.vimrc && echo "nnoremap <leader>g :let _s=@/<Bar>:%s/\s\+$//e<Bar>:let @/=_s<Bar><CR>" >> ~/.vimrc && echo "nmap <leader>- 10<C-w>-" 
+echo "nmap <leader>s :w<cr>" >> ~/.vimrc  && echo "nmap <leader>q :q!<cr>" >> ~/.vimrc && echo "nmap <leader>f <C-f>" >> ~/.vimrc  && echo "nmap <leader>b <C-b>" >> ~/.vimrc && echo "nmap <leader>w <C-w>w" >> ~/.vimrc  && echo "nmap <leader>z whdaw" >> ~/.vimrc && echo "nnoremap <leader>g :let _s=@/<Bar>:%s/\s\+$//e<Bar>:let @/=_s<Bar><CR>" >> ~/.vimrc && echo "nmap <leader>- 10<C-w>-"  >> ~/.vimrc
 
 # Set Vim's command aliases
 echo "cnoreabbrev rc e ~/.vimrc" >> ~/.vimrc  && echo "cnoreabbrev rl so ~/.vimrc" >> ~/.vimrc  && echo "cnoreabbrev nt NERDTree" >> ~/.vimrc
@@ -28,4 +28,5 @@ echo "cnoreabbrev rc e ~/.vimrc" >> ~/.vimrc  && echo "cnoreabbrev rl so ~/.vimr
 # Set Vim's command 
 echo ":command -nargs=* -complete=help H vertical belowright help <args>" >> ~/.vimrc
 
+vim +PluginInstall
 vim +PluginInstall
