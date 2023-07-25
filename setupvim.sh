56ca28b18975a4917043e6449f6a5d0d8c23884a
Alex Codeing <122148346+alexcode-cc@users.noreply.github.com>
2023-07-25 11:29:33 +0800

    Update setupvim.sh

--- setupvim.sh
+++ setupvim.sh
@@ -13,9 +13,6 @@ git clone https://github.com/alexcode-cc/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Set vimrc
 echo "set nocompatible" > ~/.vimrc && echo "filetype off" >> ~/.vimrc && echo "set rtp+=~/.vim/bundle/Vundle.vim" >> ~/.vimrc && echo "call vundle#begin()" >> ~/.vimrc && echo "Plugin 'alexcode-cc/Vundle.vim'" >> ~/.vimrc && echo "Plugin 'scrooloose/nerdtree'" >> ~/.vimrc && echo "Plugin 'mattn/emmet-vim'" >> ~/.vimrc && echo "call vundle#end()" >> ~/.vimrc && echo "filetype plugin indent on" >> ~/.vimrc  && echo "let g:user_emmet_expandabbr_key = '<tab>'" >> ~/.vimrc  && echo "syntax on" >> ~/.vimrc
 
-# Set Vim's command 
-echo ":command -nargs=* -complete=help H vertical belowright help <args>" >> ~/.vimrc
-
 # Disable Vim's up / down / left / right key
 echo "map <UP> <NOP>" >> ~/.vimrc && echo "map <DOWN> <NOP>" >> ~/.vimrc && echo "map <LEFT> <NOP>" >> ~/.vimrc  && echo "map <RIGHT> <NOP>" >> ~/.vimrc && echo "inoremap <UP> <NOP>" >> ~/.vimrc  && echo "inoremap <DOWN> <NOP>" >> ~/.vimrc && echo "inoremap <LEFT> <NOP>" >> ~/.vimrc  && echo "inoremap <RIGHT> <NOP>" >> ~/.vimrc
 
@@ -23,7 +20,9 @@ echo "map <UP> <NOP>" >> ~/.vimrc && echo "map <DOWN> <NOP>" >> ~/.vimrc && echo
 echo "nmap <leader>s :w<cr>" >> ~/.vimrc  && echo "nmap <leader>q :q!<cr>" >> ~/.vimrc && echo "nmap <leader>f <C-f>" >> ~/.vimrc  && echo "nmap <leader>b <C-b>" >> ~/.vimrc && echo "nmap <leader>w <C-w>w" >> ~/.vimrc  && echo "nmap <leader>z whdaw" >> ~/.vimrc && echo "nnoremap <leader>g :let _s=@/<Bar>:%s/\s\+$//e<Bar>:let @/=_s<Bar><CR>" >> ~/.vimrc && echo "nmap <leader>- 10<C-w>-" 
 
 # Set Vim's command aliases
-
 echo "cnoreabbrev rc e $MYVIMRC" >> ~/.vimrc  && echo "cnoreabbrev rl so $MYVIMRC" >> ~/.vimrc  && echo "cnoreabbrev nt NERDTree" >> ~/.vimrc
+
+# Set Vim's command 
+echo ":command -nargs=* -complete=help H vertical belowright help <args>" >> ~/.vimrc
 
 vim +PluginInstall
